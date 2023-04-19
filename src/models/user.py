85c810837e6a2a50d3fbcc09287d80bfef66b188
Sana Ullah <91070351+Sana-Ullah786@users.noreply.github.com>
2023-04-19 19:59:59 +0500

    Added user model created in  users.py file and used pre-commit hooks

--- src/models/user.py
+++ src/models/user.py
@@ -1,14 +1,17 @@
 from datetime import datetime
-from sqlalchemy import Boolean, mapped_column, Integer, String, DateTime, func
+
 from database import Base
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Boolean, DateTime, Integer, String, func, mapped_column
+from sqlalchemy.orm import Mapped
 
 
 class Users(Base):
     """
-    This is the user model that will be used to create the users table in the database.
-    And will be used to create the user object.
+    This is the user model that will be used to create the users table
+    in the database.
+    And will be used to create the user object
     """
+
     __tablename__ = "users"
     id: int = mapped_column(Integer(), primary_key=True, index=True)
     email: str = mapped_column(String(32), unique=True, index=True)
@@ -19,6 +22,9 @@ class Users(Base):
     contact_number: str = mapped_column(String(32))
     address: str = mapped_column(String(200))
     is_librarian: bool = mapped_column(Boolean, default=False)
-    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
-    updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), onupdate=func.now(),nullable=True)
-    
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now()
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), onupdate=func.now(), nullable=True
+    )
