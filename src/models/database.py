093d69e1a8a01b2eb1c10600c1494266aa244e47
Mustansir Muzaffar <mustansirmuzaffar@folio3.com>
2023-04-19 16:43:10 +0500

    type hint and docstring added

--- src/models/database.py
+++ src/models/database.py
@@ -1,7 +1,11 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
+from typing import Generator
 
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://mustansir:12345678@localhost/library_management"
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session, declarative_base, sessionmaker
+
+SQLALCHEMY_DATABASE_URL = (
+    "postgresql+psycopg2://mustansir:12345678@localhost/library_management"
+)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
@@ -10,7 +14,10 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 
-def get_db():
+def get_db() -> Generator[Session, None, None]:
+    """
+    A generetor function that yields the DB session
+    """
     try:
         db = SessionLocal()
         yield db
