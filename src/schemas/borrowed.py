c886f95df04b6bb26d4e3ceaf6cb7d52c51d468a
Mustansir Muzaffar <mustansirmuzaffar@folio3.com>
2023-04-25 14:51:23 +0500

    [models] Pydantic model Borrowed

---
+++ src/schemas/borrowed.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel, Field, validator
+
+
+class Borrowed(BaseModel):
+    """
+    The Pydantic Schema model for the Borrowed model
+    """
+
+    id: Optional[int] = None
+    copy_id: int = Field(title="The ID of the copy to be borrowed")
+    user_id: str = Field(title="The ID of the user who is borrowing")
+    issue_date: datetime = Field(title="Date on which book was issued")
+    due_date: datetime = Field(title="Date on which book return is due")
+    return_date: datetime = Field(title="Date on which book is returned by the user")
+
+    @validator("due_date")
+    def due_date_greater_than_issue_date(cls, v, values, **kwargs):
+        if "issue_date" in values and values["issue_date"] >= values["due_date"]:
+            raise ValueError("Due Date must be greater than Issue Date")
+
+    @validator("return_date")
+    def return_date_greater_than_issue_date(cls, v, values, **kwargs):
+        if "issue_date" in values and values["issue_date"] >= values["return_date"]:
+            raise ValueError("Return Date must be greater than Issue Date")
