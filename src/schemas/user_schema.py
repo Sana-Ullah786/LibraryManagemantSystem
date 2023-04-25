193d18417b8526a878bac748077d3759443cd8bc
Muhammad Talha Tahir <talhatahir@pk.folio3.com>
2023-04-25 14:30:43 +0500

    added Config class for user_schema

--- src/schemas/user_schema.py
+++ src/schemas/user_schema.py
@@ -29,3 +29,16 @@ class UserSchema(BaseModel):
     address: str = Field(
         min_length=0, max_length=200
     )  # mapped_column(String(200)) #noqa
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "email": "A unique email",
+                "username": "A unique username",
+                "password": "8 character string that can be used to login",
+                "first_name": "Users first name",
+                "last_name": "Users last name",
+                "contact_number": "users cellphone number",
+                "address": "users physical address",
+            }
+        }
