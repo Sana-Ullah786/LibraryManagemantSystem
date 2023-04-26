7081988f039ad9b176404f94ce7a86da62490f1b
Mustansir Muzaffar <mustansirmuzaffar@folio3.com>
2023-04-26 19:37:44 +0500

    User and Librarian Register, Token endpoints done

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 
+from routes.api import router
 from src.models import all_models
 from src.models.database import engine
 
-app = FastAPI()
-
 all_models.Base.metadata.create_all(engine)
+
+app = FastAPI()
+app.include_router(router)
