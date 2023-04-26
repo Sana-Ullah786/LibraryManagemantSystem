fb8a3721514ba357d99ad32a178ecdd600a7a6e1
Mustansir Muzaffar <mustansirmuzaffar@folio3.com>
2023-04-26 14:33:39 +0500

    Added Schema suffix to model name

--- src/schemas/borrowed.py
+++ src/schemas/borrowed.py
@@ -4,7 +4,7 @@ from typing import Optional
 from pydantic import BaseModel, Field, validator
 
 
-class Borrowed(BaseModel):
+class BorrowedSchema(BaseModel):
     """
     The Pydantic Schema model for the Borrowed model
     """
