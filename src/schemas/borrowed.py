0dcb34df9e45de59da79deb5f6da7b555f871f41
Mustansir Muzaffar <mustansirmuzaffar@folio3.com>
2023-04-26 14:30:05 +0500

    Added example

--- src/schemas/borrowed.py
+++ src/schemas/borrowed.py
@@ -25,3 +25,15 @@ class Borrowed(BaseModel):
     def return_date_greater_than_issue_date(cls, v, values, **kwargs):
         if "issue_date" in values and values["issue_date"] >= values["return_date"]:
             raise ValueError("Return Date must be greater than Issue Date")
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "id": 1,
+                "copy_id": 3,
+                "user_id": 7,
+                "issue_date": datetime(2023, 4, 26),
+                "due_date": datetime(2023, 4, 30),
+                "return_date": None,
+            }
+        }
