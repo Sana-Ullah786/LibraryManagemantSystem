d2294a603de8da3a801b83d779f6e14416a7328a
SohaibkhanFolio3 <sohaibkhan@folio3.com>
2023-05-09 17:32:48 +0500

    Book,Copy Endpoints Restructerd, added more test cases , validators

--- routes/api.py
+++ routes/api.py
@@ -1,12 +1,14 @@
 from fastapi import APIRouter
 
-from src.endpoints import auth, author, book, borrowed, copy, user
+from src.endpoints import auth, author, borrowed, user
+from src.endpoints.book import router_init as book_router
+from src.endpoints.copy import router_init as copy_router
 from src.endpoints.language import router_init as language_router
 
 router = APIRouter()
 router.include_router(auth.router)
-router.include_router(copy.router)
-router.include_router(book.router)
+router.include_router(copy_router.router)
+router.include_router(book_router.router)
 router.include_router(language_router.router)
 router.include_router(user.router)
 router.include_router(author.router)
---
+++ src/endpoints/copy/__init__.py
@@ -0,0 +1,7 @@
+from src.endpoints.copy.delete.copy_delete import copy_delete
+from src.endpoints.copy.get.get_copies import get_copies
+from src.endpoints.copy.get.get_copies_by_book_id import get_copies_by_book_id
+from src.endpoints.copy.get.get_copy_by_id import get_copy_by_id
+from src.endpoints.copy.post.copy_create import copy_create
+from src.endpoints.copy.put.copy_update import copy_update
+from src.endpoints.copy.router_init import router
