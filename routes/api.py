973662c4a90c8fd155fb93b18f76409463642cc6
Sana Ullah <91070351+Sana-Ullah786@users.noreply.github.com>
2023-05-09 14:37:31 +0500

    Update api.py

--- routes/api.py
+++ routes/api.py
@@ -1,12 +1,13 @@
 from fastapi import APIRouter
 
 
-from src.endpoints import auth, author, borrowed, language, user
+from src.endpoints import auth, author, borrowed, language, user, copy, book
 
 router = APIRouter()
 router.include_router(auth.router)
 router.include_router(language.router)
-
+router.include_router(copy.router)
+router.include_router(book.router)
 router.include_router(user.router)
 router.include_router(author.router)
 router.include_router(borrowed.router)
