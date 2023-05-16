0575be5980ef92903d6cbea2858ab705dbca2a86
Sana Ullah <91070351+Sana-Ullah786@users.noreply.github.com>
2023-05-16 14:35:05 +0500

    Added generic response fucntions that will be used in entire project.

--- src/responses.py
+++ src/responses.py
@@ -1,14 +1,21 @@
 from fastapi import status
+from fastapi.responses import JSONResponse
 
 
-def custom_response(status_code: status, details: str, data: object = None) -> dict:
+def custom_response(
+    status_code: status, details: str, data: object = None
+) -> JSONResponse:
     """
-    This function will be used to return a success response.
-    Parameters:
-        data: The data to be returned.
-        details: The details of the response.
-    Returns:
-        A dictionary containing the response.
+    Returns a custom response with the given status code, details and data
+    Parameters
+    ----------
+    status_code : The status code of the response
+    details : The details of the response
+    data : The data of the response
+    Returns
+    -------
+    A JSONResponse object
     """
-    response = {"status_code": status_code, "data": data, "details": details}
-    return response
+    return JSONResponse(
+        status_code=status_code, content={"details": details, "data": data}
+    )
