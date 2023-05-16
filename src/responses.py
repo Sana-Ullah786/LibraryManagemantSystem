026af2a477cea9f2197eab7d2d27821726d68c50
Sana Ullah <91070351+Sana-Ullah786@users.noreply.github.com>
2023-05-16 11:44:12 +0500

    Added generic response fucntions that will be used in entire project.

--- src/responses.py
+++ src/responses.py
@@ -14,9 +14,22 @@ def success_response(data: object = None, details: str = "Succes") -> dict:
     return response
 
 
-def error_response(
-    status: status = None, data: object = None, details: str = "Error"
+def custom_success_response(
+    status: status, data: object = None, details: str = "Succes"
 ) -> dict:
+    """
+    This function will be used to return a success response.
+    Parameters:
+        data: The data to be returned.
+        details: The details of the response.
+    Returns:
+        A dictionary containing the response.
+    """
+    response = {"status_code": status, "data": data, "details": details}
+    return response
+
+
+def error_response(status: status, data: object = None, details: str = "Error") -> dict:
     """
     This function will be used to return an error response.
     Parameters:
