59789bc7268f30007793b11d2fef6461f03acb27
Muhammad Talha Tahir <talhatahir@pk.folio3.com>
2023-05-22 22:20:53 +0500

    updated models according to alembic migration

--- src/models/copy.py
+++ src/models/copy.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import DateTime, ForeignKey, Integer, String, func
+from sqlalchemy import Boolean, DateTime, ForeignKey, Integer, String, func
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.models.database import Base
@@ -31,6 +31,7 @@ class Copy(Base):
     status_id: Mapped[int] = mapped_column(
         Integer(), ForeignKey("status.id"), nullable=False
     )
+    is_deleted: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
     created_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True), server_default=func.now()
     )
