836b187c74ef461189e6ccec9e307db2186ca9a2
Mustansir Muzaffar <mustansirmuzaffar@folio3.com>
2023-05-26 16:34:53 +0500

    My borrowed list Done

--- frontend/src/components/pages/UserForm.tsx
+++ frontend/src/components/pages/UserForm.tsx
@@ -20,99 +20,97 @@ function UserForm({ user, onSubmit }: Props) {
 
   return (
     <div className="modal">
-      <div className="signup-container">
-        <form onSubmit={handleSubmit(submitForm)}>
-          <div className="signup-container">
-            <div>
-              <label className="form-group" htmlFor="email">
-                Email:
-              </label>
-              <input
-                type="email"
-                id="email"
-                defaultValue={user?.email || ""}
-                {...register("email", { required: true })}
-              />
-              {errors.email && <span>This field is required</span>}
-            </div>
+      <form onSubmit={handleSubmit(submitForm)}>
+        <div className="signup-container">
+          <div>
+            <label className="form-group" htmlFor="email">
+              Email:
+            </label>
+            <input
+              type="email"
+              id="email"
+              defaultValue={user?.email || ""}
+              {...register("email", { required: true })}
+            />
+            {errors.email && <span>This field is required</span>}
+          </div>
 
-            <div>
-              <label htmlFor="username">Username:</label>
-              <input
-                type="text"
-                id="username"
-                defaultValue={user?.username || ""}
-                {...register("username", { required: true })}
-              />
-              {errors.username && <span>This field is required</span>}
-            </div>
+          <div>
+            <label htmlFor="username">Username:</label>
+            <input
+              type="text"
+              id="username"
+              defaultValue={user?.username || ""}
+              {...register("username", { required: true })}
+            />
+            {errors.username && <span>This field is required</span>}
+          </div>
 
-            <div>
-              <label htmlFor="password">New Password:</label>
-              <input
-                type="password"
-                id="password"
-                defaultValue={user?.password || ""}
-                {...register("password", { required: true })}
-              />
-              {errors.password && <span>This field is required</span>}
-            </div>
-            <div>
-              <label htmlFor="old_password"> Old Password:</label>
-              <input
-                type="password"
-                id="old_password"
-                placeholder="Required"
-                {...register("old_password", { required: true })}
-              />
-              {errors.password && <span>This field is required</span>}
-            </div>
-            <div>
-              <label htmlFor="firstName">First Name:</label>
-              <input
-                type="text"
-                id="firstName"
-                defaultValue={user?.first_name || ""}
-                {...register("first_name", { required: true })}
-              />
-              {errors.first_name && <span>This field is required</span>}
-            </div>
+          <div>
+            <label htmlFor="password">New Password:</label>
+            <input
+              type="password"
+              id="password"
+              defaultValue={user?.password || ""}
+              {...register("password", { required: true })}
+            />
+            {errors.password && <span>This field is required</span>}
+          </div>
+          <div>
+            <label htmlFor="old_password"> Old Password:</label>
+            <input
+              type="password"
+              id="old_password"
+              placeholder="Required"
+              {...register("old_password", { required: true })}
+            />
+            {errors.password && <span>This field is required</span>}
+          </div>
+          <div>
+            <label htmlFor="firstName">First Name:</label>
+            <input
+              type="text"
+              id="firstName"
+              defaultValue={user?.first_name || ""}
+              {...register("first_name", { required: true })}
+            />
+            {errors.first_name && <span>This field is required</span>}
+          </div>
 
-            <div>
-              <label htmlFor="lastName">Last Name:</label>
-              <input
-                type="text"
-                id="lastName"
-                defaultValue={user?.last_name || ""}
-                {...register("last_name", { required: true })}
-              />
-              {errors.last_name && <span>This field is required</span>}
-            </div>
+          <div>
+            <label htmlFor="lastName">Last Name:</label>
+            <input
+              type="text"
+              id="lastName"
+              defaultValue={user?.last_name || ""}
+              {...register("last_name", { required: true })}
+            />
+            {errors.last_name && <span>This field is required</span>}
+          </div>
 
-            <div>
-              <label htmlFor="contactNumber">Contact Number:</label>
-              <input
-                type="tel"
-                id="contactNumber"
-                defaultValue={user?.contact_number || ""}
-                {...register("contact_number", { required: true })}
-              />
-              {errors.contact_number && <span>This field is required</span>}
-            </div>
+          <div>
+            <label htmlFor="contactNumber">Contact Number:</label>
+            <input
+              type="tel"
+              id="contactNumber"
+              defaultValue={user?.contact_number || ""}
+              {...register("contact_number", { required: true })}
+            />
+            {errors.contact_number && <span>This field is required</span>}
+          </div>
 
-            <div>
-              <label htmlFor="address">Address:</label>
-              <textarea
-                id="address"
-                defaultValue={user?.address || ""}
-                {...register("address", { required: true })}
-              />
-              {errors.address && <span>This field is required</span>}
-            </div>
+          <div>
+            <label htmlFor="address">Address:</label>
+            <textarea
+              id="address"
+              defaultValue={user?.address || ""}
+              {...register("address", { required: true })}
+            />
+            {errors.address && <span>This field is required</span>}
           </div>
           <button type="submit">Submit</button>
-        </form>
-      </div>
+        </div>
+      </form>
     </div>
   );
 }
